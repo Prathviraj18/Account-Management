7b4a70d4be1ddd2e92fc9bd899ed077a18536a5f
RiteshSingh114 <riteshkrsingh114@gmail.com>
2022-07-25 22:06:42 +0530

    Added Login functionality
    Added Login functionality

--- src/main/java/com/capstone/accountManagementSystem/controller/LoginController.java
+++ src/main/java/com/capstone/accountManagementSystem/controller/LoginController.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capstone.accountManagementSystem.dto.User;
@@ -20,16 +22,20 @@ public class LoginController {
 	LoginService loginService; 
 	
 	@GetMapping("/login")
-	public ResponseEntity<?> getUserById() {
+	public ResponseEntity<Object> userLogin(@RequestParam("customer_id") int customer_id, @RequestParam("password") String password) {
+		System.out.println(customer_id + password);
 		
-		Optional<User> user = loginService.getUserById(1);
-		
-		if(user.isEmpty()) {
-			return ResponseEntity.noContent().build();
-		}
-		return ResponseEntity.ok(user);
-		// list ===> to json array 
-	}
-	
-	
+		 Optional<User> user = loginService.getUserById(customer_id);
+		 
+		 if(!user.isPresent()) {
+			 return new ResponseEntity<>("User doesn't exist, contact Bank Manager", HttpStatus.OK);
+		 }
+		 else {
+			 if(user.get().getPassword().equals(password)) {
+				 System.out.println("User logged in");
+				 return new ResponseEntity<>(user.get(), HttpStatus.OK);
+			 }
+		 }
+		 return new ResponseEntity<>(false, HttpStatus.OK);
+	}	
 }
