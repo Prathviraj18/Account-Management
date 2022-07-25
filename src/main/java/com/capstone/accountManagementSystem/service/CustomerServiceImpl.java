c19c466cb3e775ade66ae3e8998e5a2e90f70504
RiteshSingh114 <riteshkrsingh114@gmail.com>
2022-07-25 23:01:27 +0530

    Customer Add functionality
    Customer Add functionality

--- src/main/java/com/capstone/accountManagementSystem/service/CustomerServiceImpl.java
+++ src/main/java/com/capstone/accountManagementSystem/service/CustomerServiceImpl.java
@@ -1,8 +1,28 @@
 package com.capstone.accountManagementSystem.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.capstone.accountManagementSystem.dto.Customer;
+import com.capstone.accountManagementSystem.repo.CustomerRepository;
+import com.capstone.accountManagementSystem.repo.UserRepository;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
+	@Autowired
+	CustomerRepository customerRepository; 
+	
+	public Customer getDetailsByPan(String pancardNo)
+	{
+       return customerRepository.findByPannumber(pancardNo);
+
+	}
+    public Customer createNewCusomer(Customer customerDetails) {
+		
+		
+    	customerRepository.save(customerDetails);
+		
+		return customerDetails;
+	}
 }
