b3b8efaf603eddabec84dafdf95dac158433079d
RiteshSingh114 <riteshkrsingh114@gmail.com>
2022-07-26 15:06:48 +0530

    Mail Functionality
    Mail Functionality

--- src/main/java/com/capstone/accountManagementSystem/controller/CustomerController.java
+++ src/main/java/com/capstone/accountManagementSystem/controller/CustomerController.java
@@ -5,12 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import com.capstone.accountManagementSystem.dto.AccountTransactions;
 import com.capstone.accountManagementSystem.dto.Customer;
@@ -43,7 +46,7 @@ public class CustomerController {
 			
 				User newUser = createNewLoginCredentials(customer);
 				
-//				sendEmail(newUser,customerDetails);
+				sendMail(newUser,customerDetails);
 				
 				return ResponseEntity.created(null).body(customer);
 				
@@ -61,13 +64,20 @@ public class CustomerController {
 		
 	}
 
-	private User createNewLoginCredentials(Customer customer) {
-		User newUser = new User();
-		newUser.userId=customer.getCustomerId();
-		newUser.password="India123";
-		newUser.roleId=2;
-		customerService.createUser(newUser);
-		return newUser;
 	}
+	public void sendMail(User user,Customer customer) {
+		
+        SimpleMailMessage msg user= new SimpleMailMessage();
+        msg.setTo(customer.getEmailAddress());
+
+        msg.setSubject("----<Important>----- Login Credentials for Internet Banking");
+        msg.setText("Dear" +customer.getName()+",\nYour User Id and first time login Password is given below \nPlease make sure you change it on first login "
+        		+ "change your system generated password for first time login.\n"+ 
+        		" CustomerID: "+ user.getCustomerId() + " \nSystem generated Password:"+ user.getPassword()+ "\n \n"
+        				+ "Yours Truthful,\n Bank of Group A1 ");
+
+        javaMailSender.send(msg);
+
+}
 
 }
